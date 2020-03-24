a84d77df21078e569b0b2b7137afb934a80492db
Victoria <dzadranik@gmail.com>
2020-03-24 11:04:10 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+    #app.wrapper
+        TableTeamLead( title='Рабочий стол руководителя')
+</template>
+
+<script>
+import TableTeamLead from "./components/TableTeamLead.vue";
+
+export default {
+    name: "App",
+    components: {
+        TableTeamLead
+    }
+};
+</script>
+
+<style lang="scss">
+@import "./styles/reset.scss";
+@import "./styles/mixin/colors.scss";
+@import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
+
+html {
+	height: 100%;
+	overflow: visible;
+}
+
+body {
+	font-family: 'Open Sans', sans-serif;
+	font-size: 12px;
+	color: $color-default;
+	height: 100%;
+}
+h1 {
+	font-size: 32px;
+	margin: 10px 16px 20px;
+}
+
+.wrapper {
+	width: 1080px;
+	min-height: 100%;
+	padding: 30px 20px;
+	border-left: 100px solid $color-left-border;
+}
+</style>
---
+++ src/components/TableTeamLead.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+    div
+        h1 {{title}}
+        .dashboard
+            button.dashboard__button.active Dashboard
+            button.dashboard__button Team line up
+</template>
+
+<script>
+export default {
+    name: "TableTeamLead",
+    props: {
+        title: String
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/mixin/colors.scss";
+
+.dashboard {
+    padding: 6px 16px;
+    background: $color-grey-background;
+    margin-bottom: 22px;
+    &__button {
+        background: $color-white;
+        border-radius: 2px;
+        font-size: 14px;
+        color: $color-active-button;
+        line-height: 12px;
+        border: none;
+        outline: none;
+        padding: 17px 36px 16px;
+        margin-right: 20px;
+        &.active {
+            background: $color-background-active-button;
+            color: $color-active-button;
+        }
+    }
+}
+</style>
